d7dc65dc5f61eda0701480683890654a86e1224c
nipun3120 <apple@Apples-MacBook-Pro.local>
2021-07-24 10:30:23 +0530

    cart items count

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Cart from "./components/cart/Cart";
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartDisplay, setCartDisplay] = useState(false);
@@ -14,13 +15,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartDisplay && <Cart onHideDisplay={hideCartHandler}/>}
       <Header onCartDisplay={displayCartHandler}/>
       <main>
         <Meals />  
       </main>      
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import CartContext from '../../store/Cart-context';
+
 import CartIcon from '../cart/CartIcon';
 import classes from '../../assets/css/Layout/HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+
+    const cartCtx = useContext(CartContext);
+
+    const numberOfItems = cartCtx.items.reduce((totalItems, item)=> {
+        return totalItems + item.amount;
+    }, 0);
+
+
     return (
         <button className={classes.button} onClick={props.onClick}> 
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfItems}</span>
         </button>
     )
 }
