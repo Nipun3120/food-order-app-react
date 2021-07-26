9774cfb827ede959b58a4f55c2ad7dc1b639c951
nipun3120 <apple@192.168.1.4>
2021-07-27 01:05:16 +0530

    final

--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,12 +1,32 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartContext from '../../store/Cart-context';
 
 import CartIcon from '../cart/CartIcon';
 import classes from '../../assets/css/Layout/HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-
+    const [buttonIsHighligthed, setButtonIsHishligthed] = useState(false);
     const cartCtx = useContext(CartContext);
+    const btnClasses = `${classes.button} ${buttonIsHighligthed ? classes.bump: ''}`;
+    
+    const { items } = cartCtx;
+
+    useEffect(()=> {
+        if(items.length === 0) {
+            return;
+        }
+
+        setButtonIsHishligthed(true);
+
+        const timer = setTimeout(()=> {
+            setButtonIsHishligthed(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+        
+    }, [items]);
 
     const numberOfItems = cartCtx.items.reduce((totalItems, item)=> {
         return totalItems + item.amount;
@@ -14,7 +34,7 @@ const HeaderCartButton = (props) => {
 
 
     return (
-        <button className={classes.button} onClick={props.onClick}> 
+        <button className={btnClasses} onClick={props.onClick}> 
             <span className={classes.icon}>
                 <CartIcon />
             </span>
